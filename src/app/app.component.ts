33b723a1373bd0e18ba5d709857b49ccde96359b
Yaseen Tahir <yaseentahir85@gmail.com>
2024-03-10 18:21:08 +0100

    profile changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.onWindowScroll(); // Check initial scroll position
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = this.el.nativeElement.querySelectorAll('section');
+    console.log(sections);
+
+    sections.forEach((section: HTMLElement) => {
+      const navItem = this.el.nativeElement.querySelector(
+        `a[href="#${section.id}"]`
+      );
+      // navItem.classList.add('active');
+      // console.log(navItem);
+
+      if (navItem) {
+        const rect = section.getBoundingClientRect();
+        const topShown = rect.top >= 0;
+        const bottomShown = rect.bottom <= window.innerHeight;
+
+        if (topShown && bottomShown) {
+          navItem.classList.add('active');
+        } else {
+          navItem.classList.remove('active');
+        }
+      }
+    });
+  }
+}
